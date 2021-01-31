6d502e3824743d51dcae6ed9bf3fab5d8023e57e
irmakeris <irmakeris@sabanciuniv.edu>
2021-01-31 18:45:02 +0300

    1 file modified

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -22,47 +22,52 @@ function SearchResults(props) {
     fetch(SEARCH_API + q)
       .then(rsp => rsp.json())
       .then(data => setMovies(data.results));
-  }, [q]);
+  }, [location]);
 
   return (
     <div className="mt-4 pt-5">
       <form className="mb-5 mt-5">
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-3" />
-          <div className="col-sm-9">
-            <div className="row">
-              <div className="col">
-                <input
-                  type="text"
-                  className="form-control border border-dark"
-                  placeholder={q ? q : "Enter a movie"}
-                  name="inputted"
-                  onChange={event => setInputted(event.target.value)}
-                />
-              </div>
-              <div className="col">
-                <button
-                  onClick={Clicker}
-                  type="submit"
-                  className="btn btn-success text-light border border-light"
-                >
-                  Search
-                </button>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-3" />
+            <div className="col-sm-9">
+              <div className="row">
+                <div className="col">
+                  <input
+                    type="text"
+                    className="form-control border border-dark"
+                    placeholder={q ? q : "Enter a movie"}
+                    name="inputted"
+                    onChange={event => setInputted(event.target.value)}
+                  />
+                </div>
+                <div className="col">
+                  <button
+                    onClick={Clicker}
+                    type="submit"
+                    className="btn btn-success text-light border border-light"
+                  >
+                    Search
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-         </div>
       </form>
       <div className="container mt-5 d-flex flex-wrap">
         <div className="movie_container">
-          {q === null
-            ? ""
-            : movies
-                .filter(item => item.poster_path)
-
-                .map(item => <MovieCard key={item.id} movie={item} />)}
+          {movies
+            .filter(item => item.poster_path)
+            .filter(
+              item =>
+                !item.title.includes("Null") &&
+                !item.title.includes("Zero") &&
+                !item.title.includes("NULL")
+            )
+            .map(item => (
+              <MovieCard key={item.id} movie={item} />
+            ))}
         </div>
       </div>
     </div>
